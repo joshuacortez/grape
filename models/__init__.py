36a1f01abe419372eff73d39ceee8cbfcef1530b
Joshua Cortez <joshuacortez@cobenagroup.com>
2018-12-21 16:01:18 +0800

    Update __init__.py added more imports for hyperparameteroptimizer

--- models/__init__.py
+++ models/__init__.py
@@ -10,3 +10,10 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.linear_model import ElasticNet, ElasticNetCV
 
 import hyperopt
+
+import numpy as np
+from sklearn.model_selection import cross_val_score, KFold
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import make_pipeline
+import datetime
+from timeit import default_timer as timer
