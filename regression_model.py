419ecc40cabc7a72a0ea9d7dd5105ad0907e7534
antonyamzon <YAMZONAR4095@uwec.edu>
2019-02-04 15:30:01 +0800

    start refactoring grape

---
+++ regression_model.py
@@ -0,0 +1,158 @@
+import pandas as pd
+import xgboost as xgb
+import lightgbm as lgb
+from copy import copy
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import ElasticNet, ElasticNetCV
+
+from .param_space import parameter_space
+from .utils import _huber_approx_obj
+
+class RegressionModel:
+
+    def __init__(self, model_type):
+        '''
+        Positional Arguements:
+            model_type
+        Keyword Arguements:
+            metric: String. Optional. 'l1' or 'l2'.
+        '''
+
+        assert model_type in ["random_forest","elastic_net", "lightgbm", "lightgbm_special", "xgboost"]
+
+        self.verbosity = kwargs.get("verbosity", 0)
+        metric = kwargs.get("metric", "l2")
+
+        # NOTE: do this incase it is written differently in other implementations
+        if model_type in ["random_forest", "elastic_net", "xgboost"]:
+            if metric == "l1":
+                self._metric = "mae"
+            elif metric == "l2":
+                self._metric = "mse"
+        elif model_type == "lightgbm":
+            self._metric = "regression_{}".format(metric)
+        elif model_type == "lightgbm_special":
+            self._metric = metric
+
+        self._model_type = model_type
+
+        param_key = model_type
+        if model_type == "lightgbm_special":
+            param_key = "lightgbm"
+        self._param_space = copy(parameter_space[param_key])
+
+    def fit(self, X_train, y_train, **kwargs):
+        '''
+        Positional Arguements:
+            X_train
+            y_train
+        Keyword Arguements:
+            sample_weight
+            train_valid_folds
+        '''
+        sample_weight = kwargs.get("sample_weight", None)
+        train_valid_folds = kwargs.get("train_valid_folds", None)
+
+        # TODO: raise a more appropriate exception (NotImplemented)
+        if train_valid_folds is not None:
+            assert self._model_type == "elastic_net", "Auto CV (non-bayesian opt) only implemented for elastic_net"
+
+        if self._model_type == "elastic_net":
+            self._fit_elastic_net(X_train, y_train, **kwargs)
+        elif self._model_type == "random_forest":
+            self._fit_random_forest(X_train, y_train, **kwargs)
+        elif self._model_type in ["lightgbm", "lightgbm_special"]:
+            self._fit_lightgbm(X_train, y_train, **kwargs)
+        elif self._model_type == "xgboost":
+            self._fit_xgboost(X_train, y_train, **kwargs)
+
+    def _fit_elastic_net(self, X_train, y_train, **kwargs):
+        sample_weight = kwargs.get("sample_weight", None)
+        train_valid_folds = kwargs.get("train_valid_folds", None)
+
+        if train_valid_folds is not None:
+            self._param_space["n_jobs"] = -1
+            cv = train_valid_folds.split(X_train)
+            self.model = ElasticNetCV(**self._param_space, cv = cv)
+        else:
+            self.model = ElasticNet(**self._param_space)
+        
+        self.model.fit(X = X_train, y = y_train)
+        self.feature_importance_df = pd.DataFrame(data = {"feature_importance":model.coef_}, index = X_train.columns)
+
+    def _fit_random_forest(self, X_train, y_train, **kwargs):
+        sample_weight = kwargs.get("sample_weight", None)
+        train_valid_folds = kwargs.get("train_valid_folds", None)
+
+        self._param_space["n_jobs"] = -1
+        self._param_space["criterion"] = self._metric
+
+        self.model = RandomForestRegressor(**self._param_space)
+        self.model.fit(X = X_train, y = y_train, sample_weight = sample_weight)
+
+        self.feature_importance_df = pd.DataFrame(data = {"feature_importance":model.feature_importances_}, 
+            index = X_train.columns)
+
+    def _fit_lightgbm(self, X_train, y_train, **kwargs):
+        sample_weight = kwargs.get("sample_weight", None)
+        train_valid_folds = kwargs.get("train_valid_folds", None)
+        custom_obj = kwargs.get("custom_obj", None)
+        custom_feval = kwargs.get("custom_feval", None)
+
+        self._param_space["n_jobs"] = -1
+
+        if self._model_type == "lightgbm":
+            self._param_space["objective"] = self._metric
+            fobj = None
+            feval = None
+        if self._model_type == "lightgbm_special":
+            if self.verbosity >= 1:
+                print("Ignoring the set metric {} for lightgbm_special".format(self._metric))
+            if sample_weight is not None:
+                if self.verbosity >= 1:
+                    print("Disregarding sample_weights (for mape or mspe) for lightgbm_special")
+                sample_weight = None
+            self._param_space["metric"] = "None"
+
+            fobj = custom_obj
+            feval = custom_feval
+        
+        d_train = lgb.Dataset(data = X_train, label = y_train, weight = sample_weight)
+            
+        self.model = lgb.train(params = self._param_space, train_set = d_train, 
+            fobj = fobj, feval = feval)
+        self.feature_importance_df = pd.DataFrame(data = {"feature_importance":model.feature_importance()}, 
+            index = X_train.columns)
+
+    def _fit_xgboost(self, X_train, y_train, **kwargs):
+        sample_weight = kwargs.get("sample_weight", None)
+        train_valid_folds = kwargs.get("train_valid_folds", None)
+
+        self._param_space["silent"] = 1
+
+        if self._metric == "mae":
+            # NOTE: this probably doesn't work yet
+            obj = self._huber_approx_obj
+        if self._metric == "mse":
+            obj = None
+
+        if sample_weight is not None:
+            print("Sample weight not yet supported with the XGBoost model")
+            sample_weight = None
+
+        d_train = xgb.DMatrix(data = X_train, label = y_train, weight = sample_weight)
+
+        self.model = xgb.train(params = self._param_space, dtrain = d_train, 
+            obj=obj, num_boost_round = self._param_space.get("num_boost_round", 200))
+        self.feature_importance_df = pd.DataFrame.from_dict({"feature_importance":model.get_score(importance_type = "gain")})
+
+    def predict(self, X_):
+        if self._model_type == "xgboost":
+            X_pred = xgb.DMatrix(X_)
+        else:
+            X_pred = copy(X_)
+        
+        predictions = self.model.predict(X_pred)
+        
+        return predictions
---
+++ utils.py
@@ -0,0 +1,36 @@
+def get_sample_weight(samples, metric):
+    '''
+    Positional Arguements:
+        samples: Numpy Array. Required.
+        metric: String. 'l1' or 'l2'. Required.
+    '''
+    # TODO: Raise an appropriate error instead of AssertionError
+    assert metric in ["l1", "l2"]
+        
+    if metric == "l1":
+        # weight the observations so the training can implicitly optimize for Mean Absolute Percentage Error (MAPE) 
+        # instead of Mean Absolute Error (MAE)
+        sample_weight = samples.apply(lambda y: 1/y)
+
+    elif metric == "l2":
+        # weight the observations so the training can implicitly optimize for Mean Squared Percentage Error (MSPE) 
+        # instead of Mean Squared Error (MSE)
+        sample_weight = samples.apply(lambda y: 1/(y**2))
+        
+    # normalize the weights to sum up to 1.0
+    sample_weight = sample_weight/sample_weight.sum()
+    
+    # turn into numpy array
+    sample_weight = sample_weight.get_values()
+    
+    return sample_weight
+
+# taken from https://stackoverflow.com/questions/45006341/xgboost-how-to-use-mae-as-objective-function
+def _huber_approx_obj(preds, dtrain):
+    d = preds - dtrain.get_label() #remove .get_labels() for sklearn
+    h = 1  #h is delta in the graphic
+    scale = 1 + (d / h) ** 2
+    scale_sqrt = np.sqrt(scale)
+    grad = d / scale_sqrt
+    hess = (1 / scale) / scale_sqrt
+    return grad, hess
